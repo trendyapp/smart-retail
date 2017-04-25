81c667c79c149191432cab62cff9c01f38780584
trendyapp <ceo@trendyapp.co.kr>
2017-04-26 01:42:27 +0900

    Modified templates to display reports from database

--- reports/views.py
+++ reports/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Report
+from django.utils import timezone
 
 # Create your views here.
 
 def report_list(request):
-	return render(request, 'reports/report_list.html', {})
-	
+	reports = Report.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'reports/report_list.html', {'reports':reports})
+
+
+
